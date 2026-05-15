b2a77435d2763b4a6ba89764274dcbb6a6b6c51f
Vinicius Rodrigues da Silva <vrs20@aluno.ifal.edu.br>
2026-05-15 19:31:53 -0300

    Corrigindo o Chamado.js

--- models/Chamado.js
+++ models/Chamado.js
@@ -1,27 +1,33 @@
 class Chamado {
-    #id
-    static registroDeChamados = []
-    constructor(id, descricao){
-        this.#id = id
-        this.descricao = descricao
+    #id;
+
+    constructor(
+        id,
+        descricao,
+        categoria,
+        localizacao,
+        status = "Aberto"
+    ) {
+        this.#id = id;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.localizacao = localizacao;
+        this.status = status;
     }
-    // metodo abrirChamado dando mais informações sobre o chamado.
-    abrirChamado(categoria, localizacao, status){
-        this.categoria = categoria
-        this.localizacao = localizacao
-        this.status = status
-        // cria um objeto para adicionar no array
-        let novoChamado = {
+
+    atualizarStatus(novoStatus) {
+        this.status = novoStatus;
+    }
+
+    exibirInfo() {
+        console.log({
             id: this.#id,
             descricao: this.descricao,
             categoria: this.categoria,
             localizacao: this.localizacao,
             status: this.status
-        }
-        Chamado.registroDeChamados.push(novoChamado)
-        console.log(Chamado.registroDeChamados)
+        });
     }
 }
 
-let c1 = new Chamado(25, "Incendio em condominio")
-c1.abrirChamado("Incendio", "Rua Santo Amaro", "Médio")
+module.exports = Chamado;
